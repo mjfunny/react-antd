4a642ce5389eb380cc28d9eb83f752a09db2f26e
mjfunny <mj98986@163.com>
2017-07-26 22:07:55 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // 单独打包css
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 生成html
+
+const entry = {};
